b48f3549872a96d5e537b9cd965d42e70380c4f2
RoniDomi <roni.domi001@umb.edu>
2024-08-12 19:40:44 -0400

    test commit

--- src/main/Main.java
+++ src/main/Main.java
@@ -7,7 +7,7 @@ public class Main {
         JFrame window = new JFrame();
         window.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         window.setResizable(false);
-        window.setTitle("Dali RPG");
+        //window.setTitle("Dali RPG");
 
         GamePanel gamePanel = new GamePanel();
         window.add(gamePanel);
