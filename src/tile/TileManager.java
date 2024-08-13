a1048adf21d7f503231234b9280fdfb4f978c589
RoniDomi <roni.domi001@umb.edu>
2024-08-13 18:53:00 -0400

    Fixed player to screen center, added more assets and new map

--- src/tile/TileManager.java
+++ src/tile/TileManager.java
@@ -16,22 +16,78 @@ public class TileManager {
 
     public TileManager(GamePanel gp) {
         this.gp = gp;
-        tile = new Tile[10];
+        tile = new Tile[20];
         getTileImage();
         mapTileNum = new int[gp.maxScreenCol][gp.maxScreenRow];
-        loadMap("/maps/map00.txt");
+        loadMap("/maps/map01.txt");
     }
 
     public void getTileImage() {
         try {
+            // Grass
             tile[0] = new Tile();
             tile[0].image = ImageIO.read(getClass().getResourceAsStream("/tiles/grass1.png"));
 
+            // Wall
             tile[1] = new Tile();
             tile[1].image = ImageIO.read(getClass().getResourceAsStream("/tiles/wall1.png"));
 
+            // Water
             tile[2] = new Tile();
             tile[2].image = ImageIO.read(getClass().getResourceAsStream("/tiles/water1.png"));
+
+            // Tree
+            tile[3] = new Tile();
+            tile[3].image = ImageIO.read(getClass().getResourceAsStream("/tiles/tree1.png"));
+
+            // Full dirt block vertical
+            tile[4] = new Tile();
+            tile[4].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirt1.png"));
+
+            // Full dirt horizontal
+            tile[5] = new Tile();
+            tile[5].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirt2.png"));
+
+            // Slim dirt path vertical
+            tile[6] = new Tile();
+            tile[6].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpath1.png"));
+
+            // Slim dirt path horizontal
+            tile[7] = new Tile();
+            tile[7].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpath2.png"));
+
+            // Left dirt edge
+            tile[8] = new Tile();
+            tile[8].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpathedge1.png"));
+
+            // Right dirt edge
+            tile[9] = new Tile();
+            tile[9].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpathedge2.png"));
+
+            // Up dirt edge
+            tile[10] = new Tile();
+            tile[10].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpathedge3.png"));
+
+            // Down dirt edge
+            tile[11] = new Tile();
+            tile[11].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtpathedge4.png"));
+
+            // Dirt turn left 1
+            tile[12] = new Tile();
+            tile[12].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtturnL1.png"));
+
+            // Dirt turn left 2
+            tile[13] = new Tile();
+            tile[13].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtturnL2.png"));
+
+            // Dirt turn right 1
+            tile[14] = new Tile();
+            tile[14].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtturnR1.png"));
+
+            // Dirt turn right 2
+            tile[15] = new Tile();
+            tile[15].image = ImageIO.read(getClass().getResourceAsStream("/tiles/dirtturnR2.png"));
+
         } catch (IOException e) {
             e.printStackTrace();
         }
