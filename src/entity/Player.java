4a300f29b96adedff5d0c7e2c1260fc8bb2787ba
RoniDomi <roni.domi001@umb.edu>
2024-08-13 19:19:31 -0400

    Camera finished, added performance improvement by loading only tiles in view

--- src/entity/Player.java
+++ src/entity/Player.java
@@ -28,8 +28,8 @@ public class Player extends Entity{
     }
 
     public void setDefaultValues() {
-        worldX = gp.tileSize * 23;
-        worldY = gp.tileSize * 21;
+        worldX = gp.tileSize * 25;
+        worldY = gp.tileSize * 45;
         speed = 4;
     }
 
