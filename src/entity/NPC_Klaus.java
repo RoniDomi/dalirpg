2347d4b05cdbea019b5a2bb7966d9c690ddf6754
RoniDomi <roni.domi001@umb.edu>
2024-08-30 23:44:54 -0400

    Initializing event branch

--- src/entity/NPC_Klaus.java
+++ src/entity/NPC_Klaus.java
@@ -35,26 +35,28 @@ public class NPC_Klaus extends Entity{
     }
 
     public void setAction() {
-        actionLockCounter ++;
-        Random random = new Random();
+        if (gp.gameState == gp.playState) {
+            actionLockCounter ++;
+            Random random = new Random();
 
-        int i = random.nextInt(100) + 1;
+            int i = random.nextInt(100) + 1;
 
-        if (actionLockCounter == 120) {
-            if (i <= 25) {
-                direction = "up";
-            }
-            if (i > 25 && i <= 50) {
-                direction = "down";
-            }
-            if (i > 50 && i <= 75) {
-                direction = "left";
-            }
-            if (i > 75) {
-                direction = "right";
-            }
+            if (actionLockCounter == 120) {
+                if (i <= 25) {
+                    direction = "up";
+                }
+                if (i > 25 && i <= 50) {
+                    direction = "down";
+                }
+                if (i > 50 && i <= 75) {
+                    direction = "left";
+                }
+                if (i > 75) {
+                    direction = "right";
+                }
 
-            actionLockCounter = 0;
+                actionLockCounter = 0;
+            }
         }
     }
 
